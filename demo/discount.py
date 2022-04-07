01b941ddb57ef721fa13c88848820c67a12252fc
srikanthpragada <srikanthpragada@yahoo.com>
2022-04-07 06:50:10 +0530

    06-APR

---
+++ demo/discount.py
@@ -0,0 +1,5 @@
+# Take price and calculate discount
+
+price = int(input("Enter price :"))
+discount = price * 0.15
+print("Discount = ", discount)
---
+++ demo/hello.py
@@ -0,0 +1,5 @@
+# Say hello
+
+name = input("Enter your name :")
+
+print("Hello", name)
