ae72fe3c84501b413d0289154cc574e0caf82798
srikanthpragada <srikanthpragada@yahoo.com>
2022-04-28 07:56:28 +0530

    28-APR

--- demo/oop/doctors.py
+++ demo/oop/doctors.py
@@ -1,4 +1,7 @@
-class Doctor:
+from abc import ABC, abstractmethod
+
+
+class Doctor(ABC):
     def __init__(self, name, dept):
         self.name = name
         self.dept = dept
@@ -6,8 +9,12 @@ class Doctor:
     def __str__(self):
         return f"{self.name},{self.dept}"
 
+    @abstractmethod
+    def get_salary(self):
+        pass
 
-class ResidentDoctor (Doctor):
+
+class ResidentDoctor(Doctor):
     def __init__(self, name, dept, salary):
         super().__init__(name, dept)
         self.salary = salary
@@ -19,5 +26,22 @@ class ResidentDoctor (Doctor):
         return self.salary
 
 
+class Consultant(Doctor):
+    def __init__(self, name, dept, visits, charge):
+        super().__init__(name, dept)
+        self.visits = visits
+        self.charge = charge
+
+    def __str__(self):
+        return f"{super().__str__()},{self.visits},{self.charge}"
+
+    def get_salary(self):
+        return self.visits * self.charge
+
+
 r = ResidentDoctor("Dr. Jane", "Ped", 400000)
 print(r)
+
+c = Consultant("Dr. Dean", "GYN", 10, 1500)
+print(c)
+print(c.get_salary())
